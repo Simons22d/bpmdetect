624e3def7d9e702a9193398be033fa87eaa69b17
sanchitchawla <sanchit_sid@hotmail.com>
2016-11-22 14:43:56 +0700

    can read files now

--- bpm.py
+++ bpm.py
@@ -1,2 +1,18 @@
-#!/usr/bin/python
+import wave
+import math
+import time
+import sys
+import pywt
+import array
+import numpy
+import argparse
+from scipy import signal
+import matplotlib.pyplot as plt
 
+
+def read_wav(filename):
+    song = wave.open(filename,'rb')
+    frames = song.getnframes();
+    fs = song.getframerate()
+    samps = list(array.array('i',song.readframes(nsamps)))
+    return samps, fs
